63fb611f55da5c0018dcfc04423e0a0b0e42ef63
Dominik Klapuch <klapuchdominik@gmail.com>
2019-07-18 23:55:15 +0200

    count optimization

--- src/Db/Helper.php
+++ src/Db/Helper.php
@@ -13,7 +13,7 @@ class Helper
 
 	public static function createStringPgArray(array $array): string
 	{
-		if (\count($array) === 0) {
+		if ($array === []) {
 			return '{}';
 		}
 		return \sprintf('{\'%s\'}', \implode('\',\'', \array_map(static function ($value): string {
@@ -24,7 +24,7 @@ class Helper
 
 	public static function createPgArray(array $array): string
 	{
-		if (\count($array) === 0) {
+		if ($array === []) {
 			return '{}';
 		}
 		return \sprintf('{%s}', \implode(',', $array));
