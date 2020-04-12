e5ed7ed0dda1024d94775a0b7bce30272c0a7960
Jakub Trmota <jakub@trmota.cz>
2020-04-12 23:06:34 +0200

    Helper: fix and update SQL dump, extend dump tests

--- src/Db/Helper.php
+++ src/Db/Helper.php
@@ -45,23 +45,25 @@ class Helper
 	 */
 	public static function dump(string $sql, array $parameters = [], string $format = 'html'): string
 	{
-		static $keywords1 = 'SELECT|INSERT(?:\s+INTO)?|DELETE|UNION|FROM|WHERE|HAVING|GROUP\s+BY|ORDER\s+BY|LIMIT|OFFSET|RETURNING|SET|VALUES|LEFT\s+(OUTER\s+)?JOIN|RIGHT\s+(OUTER\s+)?JOIN|INNER\s+JOIN|FULL\s+(OUTER\s+)?JOIN|CROSS\s+JOIN|TRUNCATE|BEGIN|COMMIT|ROLLBACK(?:\s+TO\s+SAVEPOINT)?|(?:RELEASE\s+)?SAVEPOINT';
-		static $keywords2 = 'ALL|DISTINCT|IGNORE|AS|USING|ON|AND|OR|IN|IS|NOT|NULL|LIKE|ILIKE|TRUE|FALSE';
+		static $iportantKeywords = 'SELECT|INSERT(?:\s+INTO)?|DELETE|UNION|FROM|WHERE|HAVING|GROUP\s+BY|ORDER\s+BY|LIMIT|OFFSET|RETURNING|SET|VALUES|LEFT(?:\s+OUTER)?\s+JOIN|RIGHT(?:\s+OUTER)?\s+JOIN|INNER\s+JOIN|FULL(?:\s+OUTER)?\s+JOIN|CROSS\s+JOIN|TRUNCATE|BEGIN(?:\s+TRANSACTION)?|COMMIT|ROLLBACK(?:\s+TO\s+SAVEPOINT)?|(?:RELEASE\s+)?SAVEPOINT';
+		static $otherKeywords = 'ALL|DISTINCT|IGNORE|AS|USING|ON|AND|OR|IN|IS|NOT|NULL|LIKE|ILIKE|TRUE|FALSE';
+		static $variables = '\$([0-9]+)|(\'[^\']*\')';
 
 		// insert new lines
 		$sql = ' ' . $sql . ' ';
-		$sql = (string) \preg_replace(\sprintf('#(?<=[\\s,(])(%s)(?=[\\s,)])#i', $keywords1), "\n\$1", $sql); // intentionally (string), other can't be returned
+		$sql = (string) \preg_replace(\sprintf('#(?<=[\\s,(])(%s)(?=[\\s,)])#i', $iportantKeywords), "\n\$1", $sql); // intentionally (string), other can't be returned
 
 		// reduce spaces
 		$sql = (string) \preg_replace('#[ \t]{2,}#', ' ', $sql); // intentionally (string), other can't be returned
-		$sql = \wordwrap($sql, 100);
+		$sql = \wordwrap($sql, 80);
 		$sql = (string) \preg_replace("#([ \t]*\r?\n){2,}#", "\n", $sql); // intentionally (string), other can't be returned
 
 		// syntax highlight
 		$highlighter = \sprintf(
-			'#(/\\*.+?\\*/)|(\\*\\*.+?\\*\\*)|(?<=[\\s,(])(%s)(?=[\\s,)])|(?<=[\\s,(=])(%s)(?=[\\s,)=])#is',
-			$keywords1,
-			$keywords2
+			'#(/\\*.+?\\*/)|(?<=[\\s,(])(%s)(?=[\\s,)])|(?<=[\\s,(=])(%s)(?=[\\s,)=])|(%s)#is',
+			$iportantKeywords,
+			$otherKeywords,
+			$variables
 		);
 		if ($format === 'cli') {
 			if (\substr((string) \getenv('TERM'), 0, 5) === 'xterm') {
@@ -69,12 +71,12 @@ class Helper
 				$sql = (string) \preg_replace_callback($highlighter, static function (array $m): string {
 					if (isset($m[1]) && $m[1]) { // comment
 						return \sprintf("\033[1;30m%s\033[0m", $m[1]);
-					} elseif (isset($m[2]) && $m[2]) { // error
-						return \sprintf("\033[1;31m%s\033[0m", $m[2]);
-					} elseif (isset($m[3]) && $m[3]) { // most important keywords
-						return \sprintf("\033[1;34m%s\033[0m", $m[3]);
-					} elseif (isset($m[4]) && $m[4]) { // other keywords
-						return \sprintf("\033[1;32m%s\033[0m", $m[4]);
+					} elseif (isset($m[2]) && $m[2]) { // important keywords
+						return \sprintf("\033[1;34m%s\033[0m", $m[2]);
+					} elseif (isset($m[3]) && $m[3]) { // other keywords
+						return \sprintf("\033[1;32m%s\033[0m", $m[3]);
+					} elseif (isset($m[4]) && $m[4]) { // variables
+						return \sprintf("\033[1;35m%s\033[0m", $m[4]);
 					}
 					return $m[0];
 				}, $sql);
@@ -86,12 +88,12 @@ class Helper
 			$sql = (string) \preg_replace_callback($highlighter, static function (array $m): string {
 				if (isset($m[1]) && $m[1]) { // comment
 					return \sprintf('<em style="color:gray">%s</em>', $m[1]);
-				} elseif (isset($m[2]) && $m[2]) { // error
-					return \sprintf('<strong style="color:red">%s</strong>', $m[2]);
-				} elseif (isset($m[3]) && $m[3]) { // most important keywords
-					return \sprintf('<strong style="color:blue">%s</strong>', $m[3]);
-				} elseif (isset($m[4]) && $m[4]) { // other keywords
-					return \sprintf('<strong style="color:green">%s</strong>', $m[4]);
+				} elseif (isset($m[2]) && $m[2]) { // important keywords
+					return \sprintf('<strong style="color:blue">%s</strong>', $m[2]);
+				} elseif (isset($m[3]) && $m[3]) { // other keywords
+					return \sprintf('<strong style="color:green">%s</strong>', $m[3]);
+				} elseif (isset($m[4]) && $m[4]) { // variables
+					return \sprintf('<strong style="color:brown">%s</strong>', $m[4]);
 				}
 				return $m[0];
 			}, $sql);
--- tests/Unit/BasicTest.php
+++ tests/Unit/BasicTest.php
@@ -53,18 +53,35 @@ class BasicTest extends Tester\TestCase
 	public function testDumpSqlToHtml(): void
 	{
 		Tester\Assert::same(
-			"<pre class=\"dump\"><strong style=\"color:blue\">SELECT</strong> a \n<strong style=\"color:blue\">FROM</strong> b JOIN c ON c.a = b.a \n<strong style=\"color:blue\">WHERE</strong> d = 'x' \n<strong style=\"color:blue\">GROUP BY</strong> a \n<strong style=\"color:blue\">HAVING</strong> e = 2 \n<strong style=\"color:blue\">ORDER BY</strong> a \n<strong style=\"color:blue\">LIMIT</strong> 1 \n<strong style=\"color:blue\">OFFSET</strong> 2</pre>",
-			Db\Helper::dump('SELECT a FROM b JOIN c ON c.a = b.a WHERE d = $1 GROUP BY a HAVING e = 2 ORDER BY a LIMIT 1 OFFSET 2', ['x'])
+			"<pre class=\"dump\"><strong style=\"color:blue\">SELECT</strong> <strong style=\"color:green\">DISTINCT</strong> a <strong style=\"color:green\">AS</strong> column \n<strong style=\"color:blue\">FROM</strong> b JOIN c <strong style=\"color:green\">ON</strong> c.a = b.a \n<strong style=\"color:blue\">WHERE</strong> d = <strong style=\"color:brown\">'x'</strong> <strong style=\"color:green\">AND</strong> e = <strong style=\"color:brown\">'text'</strong> \n<strong style=\"color:blue\">GROUP BY</strong> a \n<strong style=\"color:blue\">HAVING</strong> e = <strong style=\"color:green\">TRUE</strong> <strong style=\"color:green\">AND</strong> f = <strong style=\"color:green\">FALSE</strong> \n<strong style=\"color:blue\">ORDER BY</strong> a \n<strong style=\"color:blue\">LIMIT</strong> 1 \n<strong style=\"color:blue\">OFFSET</strong> 2 <em style=\"color:gray\">/* comment */</em></pre>",
+			Db\Helper::dump(
+				'SELECT DISTINCT a AS column FROM b JOIN c ON c.a = b.a WHERE d = $1 AND e = \'text\' GROUP BY a HAVING e = TRUE AND f = FALSE ORDER BY a LIMIT 1 OFFSET 2 /* comment */',
+				['x']
+			)
 		);
 	}
 
 
 	public function testDumpSqlToCli(): void
 	{
-		\putenv('TERM=none'); // don't use xterm in this test, if is really used
+		\putenv('TERM=none'); // don't use xterm in this test, if it is really used
 		Tester\Assert::same(
-			"SELECT a \nFROM b JOIN c ON c.a = b.a \nWHERE d = 'x' \nGROUP BY a \nHAVING e = 2 \nORDER BY a \nLIMIT 1 \nOFFSET 2",
-			Db\Helper::dump('SELECT a FROM b JOIN c ON c.a = b.a WHERE d = $1 GROUP BY a HAVING e = 2 ORDER BY a LIMIT 1 OFFSET 2', ['x'], 'cli')
+			"SELECT DISTINCT a AS column \nFROM b JOIN c ON c.a = b.a \nWHERE d = 'x' AND e = 'text' \nGROUP BY a \nHAVING e = TRUE AND f = FALSE \nORDER BY a \nLIMIT 1 \nOFFSET 2 /* comment */",
+			Db\Helper::dump(
+				'SELECT DISTINCT a AS column FROM b JOIN c ON c.a = b.a WHERE d = $1 AND e = \'text\' GROUP BY a HAVING e = TRUE AND f = FALSE ORDER BY a LIMIT 1 OFFSET 2 /* comment */',
+				['x'],
+				'cli'
+			)
+		);
+
+		\putenv('TERM=xterm'); // and now simulate xterm
+		Tester\Assert::same(
+			"\033[1;34mSELECT\033[0m \033[1;32mDISTINCT\033[0m a \033[1;32mAS\033[0m column \n\033[1;34mFROM\033[0m b JOIN c \033[1;32mON\033[0m c.a = b.a \n\033[1;34mWHERE\033[0m d = \033[1;35m'x'\033[0m \033[1;32mAND\033[0m e = \033[1;35m'text'\033[0m \n\033[1;34mGROUP BY\033[0m a \n\033[1;34mHAVING\033[0m e = \033[1;32mTRUE\033[0m \033[1;32mAND\033[0m f = \033[1;32mFALSE\033[0m \n\033[1;34mORDER BY\033[0m a \n\033[1;34mLIMIT\033[0m 1 \n\033[1;34mOFFSET\033[0m 2 \033[1;30m/* comment */\033[0m",
+			Db\Helper::dump(
+				'SELECT DISTINCT a AS column FROM b JOIN c ON c.a = b.a WHERE d = $1 AND e = \'text\' GROUP BY a HAVING e = TRUE AND f = FALSE ORDER BY a LIMIT 1 OFFSET 2 /* comment */',
+				['x'],
+				'cli'
+			)
 		);
 	}
 
