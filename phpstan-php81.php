d7f3e38e1ca3081469e19fdeaa5ad1c0b5d59ea3
Jakub Trmota <jakub@trmota.cz>
2022-11-29 23:29:35 +0100

    CS

--- phpstan-php81.php
+++ phpstan-php81.php
@@ -9,6 +9,11 @@ return PHP_VERSION_ID < 80100
 					'path' => __DIR__ . '/src/Db/Connection.php',
 					'count' => 1,
 				],
+				[ // === We know, that this can't happen ===
+					'message' => '#^Parameter \#1 \$connection of function pg_set_error_verbosity expects resource, resource\|null given\.$#',
+					'path' => __DIR__ . '/src/Db/Connection.php',
+					'count' => 1,
+				],
 				[
 					'message' => '#^Parameter \#1 \$result of function pg_.+ expects resource, resource\|false given\.$#',
 					'path' => __DIR__ . '/tests/Integration/PgFunctionsTest.php',
@@ -20,13 +25,14 @@ return PHP_VERSION_ID < 80100
 	: [
 		'parameters' => [
 			'ignoreErrors' => [
-				'#Parameter \#\d+ \$.+ of function pg_.+ expects resource, PgSql\\\\Connection given\.#',
+				'#Parameter \#\d+ \$.+ of function pg_.+ expects PgSql\\\\Connection\|string, resource given\.#',
 				'#Parameter \#\d+ \$.+ of method Forrest79\\\\PhPgSql\\\\.+::.+\(\) expects resource, PgSql\\\\Result given\.#',
 				'#Parameter \#\d+ \$.+ of function pg_.+ expects PgSql\\\\Connection(\|null)?, resource(\|null)? given\.#',
 				'#Parameter \#\d+ \$.+ of function pg_.+ expects PgSql\\\\Result, PgSql\\\\Result\|false given\.#',
 				'#Parameter \#\d+ \$.+ of function pg_.+ expects PgSql\\\\Result, resource(\|false)? given\.#',
+				'#^Parameter \#1 \$connection of function pg_set_error_verbosity expects int\|PgSql\\\\Connection, resource(\|null)? given\.$#',
 				'#Parameter \#\d+ \$result of static method Forrest79\\\\PhPgSql\\\\Tests\\\\Integration\\\\PgFunctionsTest\:\:pgResultError\(\) expects resource\|false, PgSql\\\\Result\|false given\.#',
-				'#Property Forrest79\\\\PhPgSql\\\\.+::\$resource \(resource(\|null)?\) does not accept PgSql\\\\Connection\.#',
+				'#Property Forrest79\\\\PhPgSql\\\\.+::(\$resource|\$queryResource) \(resource(\|null)?\) does not accept PgSql\\\\(Connection|Result)\.#',
 				'#Parameter \#1 \$result of static method Forrest79\\\\PhPgSql\\\\.+::.+\(\) expects resource, PgSql\\\\Result given\.#',
 				[
 					'message' => '#Parameter \#2 \$rawValues of method Forrest79\\\\PhPgSql\\\\Db\\\\RowFactory::createRow\(\) expects array\<string, string\|null\>, array\<int\|string, string\|null\> given\.#',
@@ -34,7 +40,7 @@ return PHP_VERSION_ID < 80100
 					'count' => 1,
 				],
 				[
-					'message' => '#Parameter \#1 \$query of function pg_query expects string, string\|null given\.#',
+					'message' => '#Parameter \#1 \$connection of function pg_query expects PgSql\\\\Connection\|string, string\|null given\.#',
 					'path' => __DIR__ . '/tests/clean-dbs.php',
 					'count' => 1,
 				],
